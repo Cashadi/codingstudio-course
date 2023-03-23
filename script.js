634865e837a4c8961e581c6fa01c18134256d116
Cashadi <ichascashadi@gmail.com>
2023-03-24 00:03:43 +0700

    push

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// DOM
-
-let p = document.querySelector('p');
-
-let button = document.querySelector('button');
-let input = document.querySelector('input');
-
-button.addEventListener('click', function () {
-  let isi = input.value;
-  console.log('isi');
+$(document).ready(function () {
+  let h2 = $('h2');
+  h2.html('<i>cashadi front end developer</i>');
+  h2.append('<b> dia sangat rajin belajar FE</b>');
+  h2.prepend('hallo ');
+  h2.after('ganteng banget');
+  h2.before('<h1>hamdalah</h1>');
+  let input = $('input');
+  console.log(input.val('gak bahaya ta? kagaklah'));
+  h2.empty();
 });
