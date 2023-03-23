e60501eb898c3f41bf4cedc54ede7628c975edda
Cashadi <ichascashadi@gmail.com>
2023-03-23 22:29:41 +0700

    push

---
+++ script.js
@@ -0,0 +1,11 @@
+// DOM
+
+let p = document.querySelector('p');
+
+let button = document.querySelector('button');
+let input = document.querySelector('input');
+
+button.addEventListener('click', function () {
+  let isi = input.value;
+  console.log('input');
+});
